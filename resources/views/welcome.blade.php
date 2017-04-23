d15fd613788b8ed2a70b79674f86953ce804ed4e
anuj seharavat <anujseharavat@gmail.com>
2017-04-23 14:29:33 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Anuj Seharavat
                 </div>
 
                 <div class="links">
