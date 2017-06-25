12c68259cebd81200f08b42f78cc5993ca3bc506
anuj seharavat <anujseharavat@gmail.com>
2017-06-25 13:13:36 +0530

    Registration implementated

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,10 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionController@create');
